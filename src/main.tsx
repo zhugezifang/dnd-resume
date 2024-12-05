6a2fe6eff4e9741b43a26d5a28af8c27e48bf964
Arman Tang <td19941113@gmail.com>
2024-12-05 23:14:09 +0800

    build: analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { PageEdit } from '@/pages/edit'
 import { PagePreview } from '@/pages/preview'
+// import App from './app.tsx'
+import { Analytics } from '@vercel/analytics/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 // @ts-expect-error ignore
 import 'virtual:svg-icons-register'
 import './index.css'
-// import App from './app.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -25,6 +26,7 @@ createRoot(document.getElementById('root')!).render(
           element={<PagePreview />}
         />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
