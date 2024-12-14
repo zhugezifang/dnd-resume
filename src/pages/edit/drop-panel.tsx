8b0fa1a44ab4283c70705fb4d7da2e9ce0440929
Arman Tang <td19941113@gmail.com>
2024-12-14 15:36:08 +0800

    refactor: drop animation

--- src/pages/edit/drop-panel.tsx
+++ src/pages/edit/drop-panel.tsx
@@ -1,62 +1,92 @@
-import { useEffect, useRef } from 'react'
-import { Reorder } from 'motion/react'
-import { dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
-import { usePageStore } from '@/store/page-store.ts'
-import { ReorderItem } from '@/pages/edit/reorder-item.tsx'
-import { createWidgetsNode } from '@/components/widgets/widgets-util.ts'
 import type { WidgetType } from '@/components/widgets/widgets-util.ts'
+import { createWidgetsNode } from '@/components/widgets/widgets-util.ts'
+import { useLatest } from '@/hooks/use-latest.ts'
+import { ReorderItem } from '@/pages/edit/reorder-item.tsx'
+import { usePageStore } from '@/store/page-store.ts'
+import { dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter'
+import { Reorder } from 'motion/react'
+import { useEffect, useRef, useState } from 'react'
+import { useShallow } from 'zustand/react/shallow'
 
 const DropPanel = () => {
-  const widgets = usePageStore(state => state.widgets)
-  const updateWidgets = usePageStore(state => state.updateWidgets)
-  const setSelectedId = usePageStore(state => state.setSelectedId)
+  const { widgets, updateWidgets, selectedId, setSelectedId } = usePageStore(
+    useShallow(state => ({
+      widgets: state.widgets,
+      updateWidgets: state.updateWidgets,
+      selectedId: state.selectedId,
+      setSelectedId: state.setSelectedId,
+    })),
+  )
+  const widgetsRef = useLatest(widgets)
+  const selectedIdRef = useLatest(selectedId)
 
   const dropRef = useRef<HTMLDivElement | null>(null)
-  const widgetsRef = useRef<{ [id: string]: HTMLDivElement }>({})
+  const widgetsDomRef = useRef<{ [id: string]: HTMLDivElement }>({})
+  const widgetsLocationRef = useRef<Array<{ top: number; height: number }>>([])
+  useEffect(() => {
+    widgetsLocationRef.current = widgets
+      .map(item => widgetsDomRef.current[item.id])
+      .map(el => {
+        return {
+          top: (el.offsetParent as HTMLLIElement).offsetTop,
+          height: el.offsetHeight,
+        }
+      })
+  }, [widgets])
+
+  const [dropIndex, setDropIndex] = useState<null | number>(null)
+  const dropIndexRef = useLatest(dropIndex)
+  const getGuideLineTop = () => {
+    if (dropIndex === null) return null
+    if (dropIndex === 0) return 0
+    const baseTop = widgetsLocationRef.current[0].top
+    const rect = widgetsLocationRef.current[dropIndex - 1]
+    return rect.top - baseTop + rect.height
+  }
+  const guideLineTop = getGuideLineTop()
   // register drop event
   useEffect(() => {
     return dropTargetForElements({
       element: dropRef.current!,
-      onDragEnter: ({ source, location }) => {
-        const type = source.data.type as WidgetType
-        const newNode = createWidgetsNode(type)
-        // push to end
-        if (!widgets.length) {
-          updateWidgets([newNode])
-          return
+      onDrag: ({ location }) => {
+        if (!widgetsRef.current.length) {
+          return setDropIndex(0)
         }
         // calculate order
-        const widgetsLocation = widgets
-          .map(item => widgetsRef.current[item.id])
-          .map(el => {
-            const rect = el.getBoundingClientRect()
-            return {
-              top: rect.top,
-              height: rect.height,
-            }
-          })
+        const curY = location.current.input.clientY
+        const baseTop = widgetsDomRef.current[widgetsRef.current[0].id]!.getBoundingClientRect().top
+        const paddingTop = 16
+        const curTop = curY - baseTop + paddingTop
         let index = 0
-        while (index < widgetsLocation.length) {
-          const { top, height } = widgetsLocation[index]
-          if (location.current.input.clientY > top + height / 2) {
+        while (index < widgetsLocationRef.current.length) {
+          const { top, height } = widgetsLocationRef.current[index]
+          if (curTop > top + height / 2) {
             index++
           } else {
             break
           }
         }
-        // move
-        const _widgets = [...widgets]
-        _widgets.splice(index, 0, newNode)
+        setDropIndex(index)
+      },
+      onDragLeave: () => {
+        setDropIndex(null)
+      },
+      onDrop: ({ source }) => {
+        const type = source.data.type as WidgetType
+        const newNode = createWidgetsNode(type)
+        const _widgets = [...widgetsRef.current]
+        _widgets.splice(dropIndexRef.current!, 0, newNode)
         updateWidgets(_widgets)
         setSelectedId(newNode.id)
+        setDropIndex(null)
       },
     })
-  }, [widgets, updateWidgets, setSelectedId])
+  }, [updateWidgets, setSelectedId, widgetsRef, widgetsLocationRef, selectedIdRef, dropIndexRef])
 
   return (
     <div
       ref={dropRef}
-      className="print-wrapper rounded-2xl border"
+      className="print-wrapper relative rounded-2xl border"
     >
       <Reorder.Group
         axis="y"
@@ -66,11 +96,20 @@ const DropPanel = () => {
         {widgets.map(item => (
           <ReorderItem
             key={item.id}
-            ref={el => (widgetsRef.current[item.id] = el!)}
+            ref={el => (widgetsDomRef.current[item.id] = el!)}
             item={item}
           />
         ))}
       </Reorder.Group>
+      {guideLineTop !== null && (
+        <div
+          className="absolute left-0 top-3 flex w-full items-center"
+          style={{ transform: `translateY(${guideLineTop}px)` }}
+        >
+          <div className="h-2 w-2 rounded-full border-2 border-orange-400"></div>
+          <div className="h-0.5 flex-grow bg-orange-400 shadow-[0_0_6px_0_rgba(219,99,39,0.6)]"></div>
+        </div>
+      )}
     </div>
   )
 }
