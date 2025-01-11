ce8996454833c8b3dbf57801b29fb065c24c741b
Arman Tang <td19941113@gmail.com>
2025-01-11 23:47:34 +0800

    chore: SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { PageEdit } from '@/pages/page-edit.tsx'
 import { PagePreview } from '@/pages/page-preview.tsx'
 // import App from './app.tsx'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
@@ -27,6 +28,7 @@ createRoot(document.getElementById('root')!).render(
         />
       </Routes>
       <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>,
 )
