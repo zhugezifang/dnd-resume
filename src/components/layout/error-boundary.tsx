b80d50600893b9d0eb9dd6f8a21f549e43615dd2
Arman Tang <td19941113@gmail.com>
2025-04-13 17:03:56 +0800

    chore: error boundary

---
+++ src/components/layout/error-boundary.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean
+}
+
+export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true }
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1 className="mt-10 text-center text-2xl font-bold">Client Error</h1>
+    }
+
+    return this.props.children
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,49 @@
+import '@/i18n/index.ts'
+import '@/index.css'
+import { ErrorBoundary } from '@/components/layout/error-boundary.tsx'
+import { Toaster } from '@/components/ui/sonner'
 import { PageEdit } from '@/pages/edit/page.tsx'
 import { PagePreview } from '@/pages/preview/page.tsx'
 import { PagePrint } from '@/pages/print/page.tsx'
-// import App from './app.tsx'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
-import { Toaster } from './components/ui/sonner'
-import '@/i18n'
-import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')!
+const root = createRoot(container)
+root.render(
   <StrictMode>
-    <BrowserRouter>
-      <Toaster
-        position="top-center"
-        richColors
-        closeButton
-        offset={52}
-      />
-      <Routes>
-        <Route
-          path="/"
-          element={<PageEdit />}
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Toaster
+          position="top-center"
+          richColors
+          closeButton
+          offset={52}
         />
-        <Route
-          path="/edit"
-          element={<PageEdit />}
-        />
-        <Route
-          path="/preview"
-          element={<PagePreview />}
-        />
-        <Route
-          path="/print"
-          element={<PagePrint />}
-        />
-      </Routes>
-      <Analytics />
-      <SpeedInsights />
-    </BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<PageEdit />}
+          />
+          <Route
+            path="/edit"
+            element={<PageEdit />}
+          />
+          <Route
+            path="/preview"
+            element={<PagePreview />}
+          />
+          <Route
+            path="/print"
+            element={<PagePrint />}
+          />
+        </Routes>
+        <Analytics />
+        <SpeedInsights />
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>,
 )
